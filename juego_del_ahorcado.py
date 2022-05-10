619f23d4b8f0a0570da7c830d60be3ff01ee82e0
LuisJuarez@gmail.com <21al7207485@unedl.mx>
2022-05-10 17:31:35 -0500

    Agregamos diccionario de palabras

---
+++ juego_del_ahorcado.py
@@ -0,0 +1,20 @@
+def read():
+    letter=[]
+    with open("/home/juarez2112/juego/DATA.txt","r",encoding="utf-8") as f:
+        for line in f:
+            letter.append(line)
+    print(letter)
+
+def write():
+    pass
+    # names=["luis","pepe","Jose","Alberto","Zara"]
+    # with open("./archivos/names.txt","w",encoding="utf-8") as f:
+    #     for name in names:
+    #         f.write(name)
+    #         f.write("\n")
+def run():
+    read()
+
+
+if __name__ == '__main__':
+    run()

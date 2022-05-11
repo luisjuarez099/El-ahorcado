c747cb1c93ea5fd6c5dd46afa4eb662bc267b859
LuisJuarez@gmail.com <21al7207485@unedl.mx>
2022-05-11 16:46:00 -0500

    agregamos nuevos documentos

---
+++ prueba.py
@@ -0,0 +1,12 @@
+import random
+data=["nariz","nube","animal","carro","pez","arma","luna","tren","brazo","perro", "gato"]
+word=random.choice(data)
+print(word.upper().strip())
+
+for letter in word:
+    #print(letra, end=" ")
+    hide_word=["_"]*len(letter)
+    print(hide_word,end=" ".strip().upper())
+
+    
+

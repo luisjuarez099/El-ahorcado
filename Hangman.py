14cc7c46c14080914144e6894d4620276f6cf3d0
LuisJuarez@gmail.com <21al7207485@unedl.mx>
2022-05-10 20:40:47 -0500

    introducimos un bucle

--- Hangman.py
+++ Hangman.py
@@ -1,4 +1,5 @@
-from ctypes.wintypes import WORD
+from hmac import digest
+from pickle import TRUE
 import random 
 #Accedimos a la base de datos de las palabras.
 def read(data_read="/home/juarez2112/juego/data.txt"):
@@ -19,8 +20,22 @@ def run():
     lo que hara es usar de la base de datos de data (donde almacena las palabras)
     es que imprima una palabra random. '''
     data=read(data_read="/home/juarez2112/juego/data.txt")
-    random_word=random.choice(data)
-    print(random_word)
+    random_word=random.choice(data)#Palabra al azar de la base de datos
+    letra_ancho=[letra for letra in random_word]#crea una lista de la palbra
+    random_hide=["*"] * len(letra_ancho)#usa len para el largo de la palabra.
+    print(random_hide)
+    i=0
+    while TRUE: 
+        digita_palabra=input("Digita una palabra: ")
+        digita_palabra=digita_palabra.upper().strip()
+        assert digita_palabra.isalpha(), "No se admiten Numeros"
+        if digita_palabra == random_word:
+            print('si es la palabra')
+            break
+        else: 
+            print(f"No es la palabra.")
+        
+        
     
 if __name__=="__main__":
     run()

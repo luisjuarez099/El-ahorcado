36601fb03d3fc30124ca19af2d12f341b7c46678
LuisJuarez@gmail.com <21al7207485@unedl.mx>
2022-05-11 22:10:55 -0500

    nuevas funciones

--- prueba.py
+++ prueba.py
@@ -1,37 +1,19 @@
 import random
+import os
 def run():
-    data=["nariz","nube","animal","carro","pez","arma","luna","tren","brazo","perro", "gato"]
+    data=["nariz","nube","animal","carro","pez","arma","luna","tren","brazo","perro", "gato","cuaderno","elefante","chocolate","pulpo","ingles","agua","mexico"]
     word=random.choice(data)
-    phrase_random=[]#Palabra random almacenada como una lista. para comparar. 
+    phrase_random=[]#Palabra random almacenada como una lista.
     for x in word:
         phrase_random.append(x.upper().strip())
-    #print(phrase_random)
-
-    #--------------------Me oculta la palabra que debo de adivinar.
-    for letter in word:
-        #print(ord(letter.upper()),end="->")
-        for i in letter:
-            i=i.upper()
-            print(["__"],end=" ".upper().strip())
-    #Imprime el largo de la palabra que debo de digitar sus letras.
-    print(" ")    
-
-    frase=[]#Se almacena la palabra que vamos a introducir.
-    for i in word:
-        frase.append(input("Introduce una palabra: ").upper().strip())
-    # for j in frase:#Imprime cada palabra su codigo ascii
-    #     print(ord(j),end=" ")
-    print(frase)
-
-    #Para comparar podremos usar codigo ASCII????
-    # print(phrase_random)
-    # print(frase)
-    if set(phrase_random)==set(frase):
-        print("Son iguales")
-    else:
-        print("No son iguales")
-        print(f"La palabra era: {phrase_random}")
+    under_score=["_"]*len(phrase_random)
+    print(under_score)
     
-   
+    letras=input("Introduce una letra: ")
+    frase=[]
+    for letra in frase:
+        frase.append(letras)
+    print(letra)
+
 if __name__=='__main__':
     run()
